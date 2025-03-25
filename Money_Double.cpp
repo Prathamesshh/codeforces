4af233240485d25fd6fe1251dacc08ed044086f7
Prathamesshh <prathameshkurekar@gmail.com>
2025-03-25 22:17:53 +0530

    add money double

---
+++ Money_Double.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    for(int i=1;i<=y;i++){
+	        x = max(x*2 , x+1000);
+	    }
+	    cout<< x <<endl;
+	}
+
+}
