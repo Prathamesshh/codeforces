dc82df5d679df638159bf863183cd3256cd7215c
Prathamesshh <prathameshkurekar@gmail.com>
2025-03-25 22:18:05 +0530

    add these

---
+++ Morning_Run.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int x,y;
+	cin>>x>>y;
+	if(2*(x+y)>=1000) cout<<"yes";
+	else cout<<"no";
+
+}
---
+++ Multiples.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int C,V;
+    cin>>C>>V; 
+    if (C % V==0 || C % V==0)
+        cout<<"Multiples";
+    else
+        cout<<"No Multiples";
+    return 0;
+}
